00cc535eab6686a7aaa615c07158578321752ae6
mkp2885-afk <mkp2885@gmail.com>
2026-04-07 11:50:04 +0300

    Delete main.jsx

--- main.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import BettyApp from './BettyApp'
-
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <BettyApp />
-  </React.StrictMode>,
-)
